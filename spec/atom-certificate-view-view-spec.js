62c9d25d5c147e88697ddab3f798ff45b86525d6
Xiaoming <xiaoming.yang@mbp.local>
2016-10-21 21:34:06 +0800

    init

---
+++ spec/atom-certificate-view-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCertificateViewView from '../lib/atom-certificate-view-view';
+
+describe('AtomCertificateViewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
